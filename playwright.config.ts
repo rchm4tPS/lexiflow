3c70f43ea4584b89b7200f40d67605ca1cf66c16
rchm4tPS <rachmatpurwa393@gmail.com>
2026-04-27 11:14:38 +0700

    Set Playwright to align with CI in Github Actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,9 +43,16 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:5173',
-    reuseExistingServer: !process.env.CI,
-  },
+  webServer: [
+    {
+      command: 'npm run dev',
+      url: 'http://localhost:5173',
+      reuseExistingServer: !process.env.CI,
+    },
+    {
+      command: 'cd backend && npm run dev',
+      port: 3000,
+      reuseExistingServer: !process.env.CI,
+    }
+  ],
 });
